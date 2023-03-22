708e83436254533111648285234f746187a4dfac
StroeJohnCiprian <78986313+JohnStroe@users.noreply.github.com>
2023-03-22 19:47:02 +0200

    strat postgres

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const index = express();
 require('dotenv').config()
+const {Client} = require('pg')
+
 
 const PORT = process.env.SERVER_PORT
+const pgClient = new Client()
+
+await pgClient.connect()
 
 index.listen(PORT, () => {
     console.log(`Server listening on port:${PORT}`)
